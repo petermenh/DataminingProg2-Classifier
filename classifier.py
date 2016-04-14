612334cfa47fca05ff9ecc1015631d22ae3e0ba7
petermenh <peter.menh@gmail.com>
2016-04-14 03:17:27 -0500

    first attempt

---
+++ classifier.py
@@ -0,0 +1,26 @@
+'''
+Peter Menh
+CSE 4334
+Spring 2016
+Programming Assignment 2
+
+References used:
+http://www.nltk.org/index.html
+http://www.nltk.org/book/ch01.html
+
+Code snippets from programming assignment 1 solution
+'''
+import math
+import time
+import os
+import operator
+from math import log10, sqrt
+from collections import Counter
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+import pandas
+import numpy
+import matplotlib.pyplot as plt
+import csv as csv
+

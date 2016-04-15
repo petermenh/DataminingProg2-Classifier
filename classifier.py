041c1cf00188ff5ad78b82aae5f143d8399ce8ea
petermenh <peter.menh@gmail.com>
2016-04-14 20:18:14 -0500

    added a bunch of stuff
    hopefully close to done

--- classifier.py
+++ classifier.py
@@ -24,3 +24,186 @@ import numpy
 import matplotlib.pyplot as plt
 import csv as csv
 
+mytokenizer = RegexpTokenizer(r'[a-zA-Z0-9]+')
+stemmer = PorterStemmer()
+sortedstopwords = sorted(stopwords.words('english'))
+total_puid_prod_des = []
+attTermDict = {}
+attPuidDict = {}
+prodDesTermDict = {}
+prodDesPuidDict = {}
+trainPuidDict = {}
+puidDict = {}
+attVectorLength = 0
+prodDesVectorLength = 0
+brandTok = []
+brandFound = False 
+brand = 'brand'
+tf = 'tf'
+prior = 'prior'
+cProb = 'cProb'
+totalTerms = 'totalTerms'
+uniqueTerms = 'uniqueTerms'
+terms = 'terms'
+totRel = 'totRel'
+aveRel = 'aveRel'
+numofP = 'numofP'
+
+def tokenize(doc):
+    tokens = mytokenizer.tokenize(doc)
+    lowertokens = [token.lower() for token in tokens]
+    filteredtokens = [stemmer.stem(token) for token in lowertokens if not token in sortedstopwords]
+    return filteredtokens
+
+
+#---------------Main-----------------
+
+os.system('cls')
+#-----train
+print('Training...')
+start_time = time.time()
+train_csv = pandas.read_csv('train.csv', encoding="ISO-8859-1")
+
+
+for i in range(0,len(train_csv)):
+
+    if type(train_csv.search_term[i])==float:
+        if math.isnan(csvAtt.name[i]):
+            searchTok = tokenize(" ")
+    else:
+        searchTok = tokenize(train_csv.search_term[i])
+
+    if type(train_csv.product_title[i])==float:
+        if math.isnan(csvAtt.name[i]):
+            prod_titleTok = tokenize(" ")
+    else:
+        prod_titleTok = tokenize(train_csv.product_title[i])
+
+    train_termsTok = searchTok+prod_titleTok
+
+    if train_csv.product_uid[i] not in trainPuidDict:
+        trainPuidDict[train_csv.product_uid[i]] = {terms:train_termsTok,numofP:1, totRel:train_csv.relevance[i], aveRel:0}
+        tempAve = trainPuidDict[train_csv.product_uid[i]][totRel]/(3*trainPuidDict[train_csv.product_uid[i]][numofP])
+        trainPuidDict[train_csv.product_uid[i]][aveRel] = tempAve
+    else:
+        trainPuidDict[train_csv.product_uid[i]][terms] = trainPuidDict[train_csv.product_uid[i]][terms]+train_termsTok
+        trainPuidDict[train_csv.product_uid[i]][numofP] +=1
+        trainPuidDict[train_csv.product_uid[i]][totRel] += train_csv.relevance[i]
+        tempAve = trainPuidDict[train_csv.product_uid[i]][totRel]/(3*trainPuidDict[train_csv.product_uid[i]][numofP])
+        trainPuidDict[train_csv.product_uid[i]][aveRel] = tempAve
+print('.')
+print('.')
+print('.')
+print('Training time: ', time.time()-start_time)
+print()
+
+
+#-----Attribute
+print('Start attribute read')
+start_time = time.time()
+count = 0
+att_csv = pandas.read_csv('attributes.csv', sep=",", encoding="ISO-8859-1")
+print('%0 done...')
+
+for i in range(0,len(att_csv)):
+    count +=1
+    brandTok = []
+
+    if count == 100000:
+        print('%{0:.0f} done...'.format((i/len(att_csv))*100) )
+        count = 0
+
+    if type(att_csv.name[i])==float:
+        if math.isnan(att_csv.name[i]):
+            nameTok = tokenize(" ")
+    elif att_csv.name[i]=='MFG Brand Name':
+        if type(att_csv.value[i])==float:
+            if math.isnan(att_csv.value[i]):
+                valueTok = tokenize(" ")
+        else:
+            brandTok = tokenize(att_csv.value[i])
+
+    if type(att_csv.value[i])==float:
+        if math.isnan(att_csv.value[i]):
+            valueTok = tokenize(" ")
+    else:
+        valueTok = tokenize(att_csv.value[i])
+
+    if att_csv.product_uid[i] not in puidDict:
+        puidDict[att_csv.product_uid[i]] = {terms:valueTok, brand:brandTok}
+    else:
+        puidDict[att_csv.product_uid[i]][terms] += valueTok
+        puidDict[att_csv.product_uid[i]][brand] += brandTok
+
+print('%100 done')
+print('Attribute time: ', time.time()-start_time)
+print()
+
+
+#-----Product Description
+print('Start product description read')
+start_time = time.time()
+count = 0
+pro_des_csv = pandas.read_csv('product_descriptions.csv', sep=",", encoding="ISO-8859-1")
+print('%0 done...')
+
+for i in range(0,len(pro_des_csv)):
+    count +=1
+
+    if count == 100000:
+        print('%{0:.0f} done...'.format((i/len(pro_des_csv))*100) )
+        count = 0
+
+    if type(pro_des_csv.product_description[i])==float:
+        if math.isnan(pro_des_csv.product_description[i]):
+            valueTok = tokenize(" ")
+    else:
+        valueTok = tokenize(pro_des_csv.product_description[i])
+
+    if pro_des_csv.product_uid[i] not in puidDict:
+        puidDict[pro_des_csv.product_uid[i]] = {terms:valueTok, brand:[]}
+    else:
+        puidDict[pro_des_csv.product_uid[i]][terms] += valueTok
+
+print('%100 done')
+print('Product description time: ', time.time()-start_time)
+print()
+
+#-----read test and output result
+print('Start testing and output results')
+start_time = time.time()
+
+test_csv = pandas.read_csv('test.csv', sep=",", encoding="ISO-8859-1")
+
+with open('subtest.csv', 'w', newline='') as outfile:
+    writer = csv.writer(outfile, delimiter=',', quotechar = "'")
+    writer.writerow(['"id"']+['"relevance"'])
+    for i in range(0,len(test_csv)):
+        score = 1
+        searchTok = tokenize(test_csv.search_term[i])
+        test_id = test_csv.id[i]
+        test_puid = test_csv.product_uid[i]
+
+        for t in searchTok:
+            prior = 0
+            if t in puidDict[test_puid][brand]:
+                score +=1
+                if score >= 3.0:
+                    score = 3.0
+            else:
+                if t in trainPuidDict[train_csv.product_uid[i]][terms]:
+                    prior = trainPuidDict[train_csv.product_uid[i]][aveRel]
+
+                tTop = puidDict[test_puid][terms].count(t)+1
+                tBot = len(puidDict[test_puid][terms]) + len(set(puidDict[test_puid][terms]))
+                condProb = tTop/tBot
+                score += prior * condProb
+                if score >= 3:
+                    score = 3.0
+                print(score)
+
+        writer.writerow([test_id, '%.2f'%score])
+
+print('test time: ', time.time()-start_time)
+print()
+print('Results outputted')
